4fd2819690bbc5f4e2600a49ff5fab848dfcffdd
Deshan <deshanekanayake2003@gmail.com>
2025-04-29 22:30:24 +0100

    created engineer profile

--- urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("login/", views.local_account_login, name="local_account_login"),
-    path("success/", lambda request: render(request, "success.html"), name="success"),
-]
